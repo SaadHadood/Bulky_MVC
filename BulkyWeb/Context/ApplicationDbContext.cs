d062d7206a4a35e052bf0ef496db7e7bc06c4e60
Saad Hadood <Saad-96-08@hotmail.com>
2024-10-30 09:43:55 +0100

    DatabaseAdded

---
+++ BulkyWeb/Context/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using BulkyWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyWeb.Context;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+        
+    }
+
+    public DbSet<CategoryModel> Categories { get; set; }
+}
---
+++ BulkyWeb/Models/CategoryModel.cs
@@ -0,0 +1,9 @@
+﻿namespace BulkyWeb.Models;
+
+public class CategoryModel
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = null!;
+    public int DisplayOrder { get; set; }
+
+}
