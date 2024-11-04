4be067f4ddba3ffe7572f67ce138ff744ac81f32
Saad Hadood <Saad-96-08@hotmail.com>
2024-11-04 16:01:32 +0100

    CRUD

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -16,6 +16,7 @@ public class CategoryController : Controller
         return View(objCategoryList);
     }
 
+    //Create
     public IActionResult CreateCategory()
     {
         return View();
@@ -32,23 +33,59 @@ public class CategoryController : Controller
         {
             _db.Categories.Add(obj);
             _db.SaveChanges();
+            TempData["success"] = "Category Created successfully";
             return RedirectToAction("Index");
         }
         return View();
 
     }
-    [HttpDelete]
-    public IActionResult DeleteCategory(int id)
-    {
 
-        var category = _db.Categories.FirstOrDefault(c => c.Id == id);
-        if (category != null) 
+    //Edit
+    public IActionResult EditCategory(int? id) 
+    {
+        if (id == null || id == 0) { return NotFound(); }
+        CategoryModel? CategoryFromDb = _db.Categories.FirstOrDefault(c => c.Id == id);
+
+        if (CategoryFromDb == null) { return NotFound(); }
+
+        return View(CategoryFromDb);
+    }
+
+    [HttpPost]
+    public IActionResult EditCategory(CategoryModel obj) 
+    { 
+        if (ModelState.IsValid)
         {
-            _db.Categories.Remove(category);
+            _db.Categories.Update(obj);
             _db.SaveChanges();
+            TempData["success"] = "Category Edit successfully";
             return RedirectToAction("Index");
         }
         return View();
+    }
+
+
+    //Delete
+    public IActionResult DeleteCategory(int? id)
+    {
+        if (id == null || id == 0) { return NotFound(); }
+        CategoryModel? CategoryFromDb = _db.Categories.FirstOrDefault(c => c.Id == id);
+
+        if (CategoryFromDb == null) { return NotFound(); }
+
+        return View(CategoryFromDb);
+    }
+    [HttpPost, ActionName ("DeleteCategory")]
+    public IActionResult DeleteCategoryPost(int? id)
+    {
+        CategoryModel? obj = _db.Categories.FirstOrDefault(c => c.Id == id);
+
+        if (obj == null) { return NotFound(); }
+
+        _db.Categories.Remove(obj);
+        _db.SaveChanges();
+        TempData["success"] = "Category Deleted successfully";
+        return RedirectToAction("Index");
     }
 
 }
