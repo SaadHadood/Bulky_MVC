3044c5c652d1ecda28409f74f45124f5bd18a82d
Saad Hadood <Saad-96-08@hotmail.com>
2024-11-08 07:43:02 +0100

    UIUpdate

---
+++ Bulky.DataAccess/Context/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using BulkyWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyWeb.Context;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+        
+    }
+
+    public DbSet<CategoryModel> Categories { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<CategoryModel>().HasData(
+            
+                new CategoryModel { Id = 1, Name = "Action", DisplayOrder = 1 },
+                new CategoryModel { Id = 2, Name = "SciFi", DisplayOrder = 2 },
+                new CategoryModel { Id = 3, Name = "History", DisplayOrder = 3 }
+
+        );
+    }
+}
