9a3b6f4fedf4356a8b9171b1dc88484ef1c4d1cf
Saad Hadood <Saad-96-08@hotmail.com>
2024-12-02 09:40:23 +0100

    HomePageListProduct

--- BulkyWeb/Areas/Customer/Controllers/HomeController.cs
+++ BulkyWeb/Areas/Customer/Controllers/HomeController.cs
@@ -1,12 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Bulky.DataAccess.Repository.IRepository;
+using Bulky.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BulkyWeb.Areas.Customer.Controllers;
 
 [Area("Customer")]
 public class HomeController : Controller
 {
+    private readonly IUnitOfWork _unitOfWork;
+
+    public HomeController(IUnitOfWork unitOfWork)
+    {
+        _unitOfWork = unitOfWork;
+    }
     public IActionResult Index()
     {
-        return View();
+        IEnumerable<ProductModel> productList = _unitOfWork.Product.GetAll(includeProperties :"Category");
+        return View(productList);
     }
 }
