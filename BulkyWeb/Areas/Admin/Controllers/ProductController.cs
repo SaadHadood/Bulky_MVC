6e2ecf17e7850c62d05d4773149f096fda16ab7c
Saad Hadood <Saad-96-08@hotmail.com>
2024-12-13 09:33:21 +0100

    UI Register and Login

--- BulkyWeb/Areas/Admin/Controllers/ProductController.cs
+++ BulkyWeb/Areas/Admin/Controllers/ProductController.cs
@@ -1,6 +1,8 @@
 ﻿using Bulky.DataAccess.Repository.IRepository;
 using Bulky.Models;
 using Bulky.Models.ViewModels;
+using Bulky.Utility;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.Build.Tasks.Deployment.Bootstrapper;
@@ -9,6 +11,8 @@ using System.Collections.Generic;
 namespace BulkyWeb.Areas.Admin.Controllers;
 
 [Area("Admin")]
+[Authorize(Roles = SD.Role_Admin)]
+
 public class ProductController : Controller
 {
     private readonly IUnitOfWork _unitOfWork;
